66c183f90ae63cd46032094723f62239934f7e80
Zhou Zhenglong <zlzhou.sh@outlook.com>
2025-12-31 11:36:14 +0800

    release

--- workflow/init.js
+++ workflow/init.js
@@ -11,6 +11,13 @@ const utils = require('./utils');
     await utils.runCommand('node', ['./workflow/update-repo.js', forceFlag].filter(Boolean));
 
     await utils.runCommand('npm run install:engine');
+    await utils.runCommand('npm run generate:dts');
+
+    const platforms = ['bytedance', 'sud', 'wechat', 'wechat-playable'];
+    for (const platform of platforms) {
+        await utils.runCommand(`npm i --prefix ./packages/platforms/${platform}`);
+        await utils.runCommand(`npm run build --prefix ./packages/platforms/${platform}`);
+    }
 
     console.log('\n初始化完成\n');
 })();
